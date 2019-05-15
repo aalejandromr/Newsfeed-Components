ad1fbaf40f705eb0b39f77a9e437c425e60a4d42
Jose Mejia <aalejandro.mreyes@icloud.com>
2019-05-15 18:13:22 -0400

    Get Stretch Goal Done

--- Article/Article.js
+++ Article/Article.js
@@ -1,45 +1,55 @@
 // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
-// let article_wrapper = document.querySelector(".articles");
+let article_wrapper = document.querySelector(".articles");
 
 class Article {
-  constructor(domElement) {
-    // this.header = attributes.header;
-    // this.date = attributes.date;
-    // this.paragraph = attributes.paragraph;
-    // this.allParagraph = attributes.allParagraph;
-    // let h2 = document.createElement("h2");
-    // let div = document.createElement("div");
-    // let p = document.createElement("p");
-    // h2.textContent = this.header;
-    // p.textContent = this.date;
-    // div.className += "article";
-    // p.className += "date";
-    // div.appendChild(h2);
-    // div.appendChild(p);
-    // this.allParagraph.forEach(function(pa){
-    //   let tempP = document.createElement("p");
-    //   tempP.textContent = pa;
-    //   div.appendChild(tempP);
-    //   // console.log(pa);
-    // });
+  constructor(attributes) {
+    this.header = attributes.header;
+    this.date = attributes.date;
+    this.paragraph = attributes.paragraph;
+    this.allParagraph = attributes.allParagraph;
+    let h2 = document.createElement("h2");
+    this.div = document.createElement("div");
+    let p = document.createElement("p");
+    let span = document.createElement("span");
+    span.textContent = "Expand";
+    h2.textContent = this.header;
+    p.textContent = this.date;
+    this.div.className += "article";
+    p.className += "date";
+    span.className += "expandButton";
+    this.div.appendChild(h2);
+    this.div.appendChild(p);
+    this.div.appendChild(span);
     
-    // article_wrapper.appendChild(div);
+    span.addEventListener("click", () => this.expandArticle());
+    article_wrapper.appendChild(this.div);
+    // this.addAllParagraph = this.addAllParagraph.bind(this);
+    this.addAllParagraph(this.allParagraph);
     // assign this.domElement to the passed in domElement
-    this.domElement = domElement;
+    // this.domElement = domElement;
     // create a reference to the ".expandButton" class. 
-    this.expandButton = this.domElement.querySelector('.expandButton');
+    // this.expandButton = this.domElement.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = "expand";
+    // this.expandButton.textContent = "expand";
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener("click", () => this.expandArticle());
+    // this.expandButton.addEventListener("click", () => this.expandArticle());
     // console.log(this);
   }
 
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle("article-open");
-    this.domElement.classList.toggle("close");
+    // this.domElement.classList.toggle("article-open");
+    this.div.classList.toggle("article-open");
     // console.log(this.domElement);
+  }
+
+  addAllParagraph(allParagraph){
+    allParagraph.forEach(function(pa){
+      let tempP = document.createElement("p");
+      tempP.textContent = pa;
+      this.div.appendChild(tempP);
+      // console.log(this);
+    }.bind(this));
   }
 }
 
@@ -55,10 +65,31 @@ article as a parameter to the Article class.
 
 let articles = document.querySelectorAll(".article");
 
-articles.forEach( article => new Article(article));
-// new Article({
-//   header: "Header 1",
-//   date: "The Date",
-//   paragraph: "lorem lorem lorem",
-//   allParagraph: ["test1", "test2", "test3"]
-// })
+// articles.forEach( article => new Article(article));
+new Article({
+  header: "Header 1",
+  date: "The Date",
+  paragraph: "lorem lorem lorem",
+  allParagraph: ["aslkdjalskdj kqwelqjwekq", "test2", "test3"]
+})
+
+new Article({
+  header: "Header 1",
+  date: "The Date",
+  paragraph: "lorem lorem lorem",
+  allParagraph: ["test1", "test2", "test3"]
+})
+
+new Article({
+  header: "Header 1",
+  date: "The Date",
+  paragraph: "lorem lorem lorem",
+  allParagraph: ["test1", "test2", "test3"]
+})
+
+new Article({
+  header: "Header 1",
+  date: "The Date",
+  paragraph: "lorem lorem lorem",
+  allParagraph: ["test1", "test2", "test3"]
+})
